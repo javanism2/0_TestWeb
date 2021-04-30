abac4ccbc91c2f5b60c3845c5b861a63d931976c
javanism <javanism@hanmail.net>
2021-04-30 11:28:37 +0900

    [jes]EC2에서 발급받은 보안 추가

---
+++ src/main/java/test/jes/web/controller/SessionList.java
@@ -0,0 +1,24 @@
+package test.jes.web.controller;
+
+import java.util.ArrayList;
+
+import javax.websocket.Session;
+
+public class SessionList {
+	
+	public ArrayList<Session> list=new ArrayList<Session>();
+	
+	static private SessionList instance;
+	
+	private SessionList() {}
+	
+	public static SessionList getInstance() {
+		if(instance==null) {
+			instance=new SessionList();
+		}
+		return instance;
+	}
+	
+	
+
+}
